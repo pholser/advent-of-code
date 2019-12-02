70528781a15962884e370a2c92352ecc63df9b70
Paul Holser <holser@railroad19.com>
2019-12-02 10:31:23 -0600

    Day 1

--- 2019/day/1/day1.clj
+++ 2019/day/1/day1.clj
@@ -1,12 +1,22 @@
 (defn fuel-for-module-launch [mass]
   (- (quot mass 3) 2))
 
+(defn corrected-fuel-for-module-launch [mass]
+  (let [base-fuel (fuel-for-module-launch mass)]
+    (if (neg? base-fuel)
+      0
+      (+ base-fuel (corrected-fuel-for-module-launch base-fuel)))))
+
 (defn total-fuel-requirements [masses]
   (reduce + masses))
 
-(defn day1 []
+(defn day1 [fuel-calc]
   (with-open [masses-in (clojure.java.io/reader "./masses.txt")]
     (total-fuel-requirements
       (map
-        (comp fuel-for-module-launch #(Integer/parseInt %))
-        (line-seq masses-in))))) 
+        (comp fuel-calc #(Integer/parseInt %))
+        (line-seq masses-in)))))
+
+(defn day1-part1 [] (day1 fuel-for-module-launch))
+(defn day1-part2 [] (day1 corrected-fuel-for-module-launch))
+
