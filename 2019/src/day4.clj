d220e81e661b3b78561df823302d0fe014eb9550
Paul Holser <holser@railroad19.com>
2019-12-04 15:23:15 -0600

    Day 4 part 2

--- 2019/src/day4.clj
+++ 2019/src/day4.clj
@@ -5,6 +5,9 @@
   (every?
     (fn [[d1 d2]] (<= (compare d1 d2) 0))
     (partition 2 1 (str n))))
+(defn part2-stipulation? [n]
+  (let [adjacents (map first (re-seq #"(.)\1*" (str n)))]
+    (> (count (filter (fn [s] (= 2 (count s))) adjacents)) 0)))
 
 (defn inclusive-range [start end] (range start (inc end)))
 
@@ -13,3 +16,14 @@
     (filter
       (every-pred six-digits? has-adjacent-same-digits? increasing-digits?)
       (inclusive-range 138307 654504))))
+
+(defn day4-part2 []
+  (count
+    (filter
+      (every-pred
+        six-digits?
+        has-adjacent-same-digits?
+        increasing-digits?
+        part2-stipulation?)
+      (inclusive-range 138307 654504))))
+
