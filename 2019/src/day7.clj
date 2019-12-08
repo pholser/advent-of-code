a9f1ed0c5fae314402685e5a89f0ac4ce2322a0a
Paul Holser <holser@railroad19.com>
2019-12-08 00:12:31 -0600

    day 7 part 1 finished

--- 2019/src/day7.clj
+++ 2019/src/day7.clj
@@ -17,7 +17,7 @@
       (throw
         (IllegalArgumentException.
           (format "Unrecognized parameter mode: %d" indicator)))))
-(defn mode-at [modes pos] (do (println "mode at pos " pos (nth modes pos value-at)) (nth modes pos value-at)))
+(defn mode-at [modes pos] (nth modes pos value-at))
 (defn arg-at [program start modes index]
   ((mode-at modes index) program (nth program (+ start (+ index 1)))))
 (defn store-at [program pos val] (replace-at program pos (str val)))
@@ -27,40 +27,32 @@
          inval2 (arg-at program start modes 1)
          outpos (Integer/parseInt (value-of-memory-cell program (+ start 3)))
          result (op inval1 inval2)]
-    (do (println inval1 " " op " " inval2 " = " result ", store at " outpos)
-    (store-at program outpos result)))   )
+    (store-at program outpos result)))
 
 (defn execute-store-op [program input start modes]
   (let [val (Integer/parseInt (poll! input))
          outpos (Integer/parseInt (value-of-memory-cell program (+ start 1)))]
-    (do   (println "reading   " val ", storing at " outpos)
-    (store-at program outpos val)))    )
+    (store-at program outpos val)))
 
 (defn execute-print-op [program output start modes]
   (let [val (arg-at program start modes 0)]
-    (do   (println "outputting " val)
+    (do
       (put! output (str val))
       program)))
 
 (defn jump-dest [op program start modes]
   (let [val (arg-at program start modes 0)
          newptr (arg-at program start modes 1)]
-    (do (println "testing value " val ", newptr = " newptr)
     (if
       (op 0 val)
-      (do
-        (println "since " val " " op " 0, jumping to " newptr)
-        newptr)
-      (do
-        (println "since not " val " " op " 0, jumping to " (+ start 3))
-        (+ start 3)))))   )
+      newptr
+      (+ start 3))))
 
 (defn execute-compare-op [op program start modes]
   (let [inval1 (arg-at program start modes 0)
          inval2 (arg-at program start modes 1)
          outpos (Integer/parseInt (value-of-memory-cell program (+ start 3)))]
-    (do (println "storing " (if (op inval1 inval2) "1" "0") " at " outpos)
-    (store-at program outpos (if (op inval1 inval2) "1" "0"))))   )
+    (store-at program outpos (if (op inval1 inval2) "1" "0"))))
 
 (defn parse-instruction [instruction]
   (case instruction
@@ -83,7 +75,6 @@
 
 (defn run-at [program input output start]
   (let [instruction (parse-instruction (nth program start))]
-    (do (println "run instruction " instruction " at addr " start)
     (case (:opcode instruction)
       "01"
         (run-at
@@ -137,7 +128,6 @@
       (throw
         (IllegalArgumentException.
           (format "Weird opcode at %d: %s" start (:opcode instruction)))))))
-)
 
 (defn run [program input output]
   (first (run-at program input output 0)))
@@ -174,7 +164,6 @@
               amp4 (amplifier (read-program instructions) chan34 chan45)
               amp5 (amplifier (read-program instructions) chan45 out)]
               (do
-                (println "phase settings " ph1 ph2 ph3 ph4 ph5)
                 (amp-input amp1 ph1)
                 (amp-input amp1 0)
                 (amp-input amp2 ph2)
@@ -187,5 +176,5 @@
                 (amp-run amp4)
                 (amp-run amp5)
                 (Integer/parseInt (poll! (:output amp5))))))
-          (combos/selections (range 0 1) 5)))))) 
+          (combos/permutations (range 0 5)))))))
 
