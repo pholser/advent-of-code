a9eebd3e7b829f6f2ee818474b8aa259732986dc
Paul Holser <holser@railroad19.com>
2019-12-10 12:02:07 -0600

    day 10 part 1

--- 2019/src/day10.clj
+++ 2019/src/day10.clj
@@ -1,5 +1,8 @@
 (require '[clojure.string :as str])
 
+(defn x [coord] (first coord))
+(defn y [coord] (second coord))
+
 (defn ys-with-xs [rows]
   (map-indexed vector (map #(map-indexed vector %) rows)))
 
@@ -14,11 +17,40 @@
 (defn asteroid-coordinates [rows]
   (apply concat (map asteroid-coordinates-in-row (ys-with-xs rows))))
 
+; For each asteroid a:
+;   Check each other asteroid b to see if, on the line between a and b,
+;     any other asteroid intervenes. If not, a has clear line-of-sight to b.
+
+(defn collinear? [a b c]
+  (=
+    (* (- (x b) (x a)) (- (y c) (y a)))
+    (* (- (x c) (x a)) (- (y b) (y a)))))
+(defn within? [p q r]
+   (and
+     (or (<= p q r) (<= r q p))
+     (or (not= p r) (= p q r))))
+(defn between? [a c b]
+  (and
+    (collinear? a b c)
+    (if (= (x a) (x b))
+      (within? (y a) (y c) (y b))
+      (within? (x a) (x c) (x b)))))
+(defn clear-line-of-sight? [a b asteroids]
+  (=
+    0
+    (count
+      (filter
+        (fn [c] (and (not= a c) (not= b c) (between? a c b)))
+        asteroids))))
+(defn visible-from [a asteroids]
+  (filter #(clear-line-of-sight? a % asteroids) asteroids))
+(defn part1-answer [asteroids]
+  (apply max
+    (map (comp count #(visible-from % asteroids)) asteroids)))
+
 (defn day10-part1 [file-name]
   (with-open
     [rows-in (clojure.java.io/reader file-name)]
-    (let [
-      rows (doall (line-seq rows-in))
-      ]
-      (asteroid-coordinates rows))))
+    (let [rows (doall (line-seq rows-in))]
+      (part1-answer (asteroid-coordinates rows)))))
 
